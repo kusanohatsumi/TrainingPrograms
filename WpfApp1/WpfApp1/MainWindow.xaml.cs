86a1f25d2bdc4189fdca41729f8b88e0725c87a9
Kusano Hatsumi <kusano.hatsumi@gmail.com>
2024-04-24 17:04:10 +0900

    feat:13章のデータ提出
    電卓の実装練習

---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        readonly CalcViewModel ViewModel = new();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this.ViewModel;
+        }
+
+
+        private void Num_Click(object sender, RoutedEventArgs e)
+        {
+            var btn = (Button)sender;
+            this.ViewModel.NumCommand(btn.Content);
+        }
+        private void Cmd_Click(object sender, RoutedEventArgs e)
+        {
+            var btn = (Button)sender;
+            this.ViewModel.CmdCommand(btn.Content);
+
+        }
+
+    }
+}
